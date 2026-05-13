fb2622f1f6b487842a44b5de6893ccf494177c41
Samwel Kamau <kamausamwel281@gmail.com>
2026-05-13 12:46:09 +0300

    Revert "Feat:Implement initial dashboard and homepage layouts"

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+# StudySync Student Planner
 
+A student study planner system built with React, Tailwind CSS, React Router, and JSON Server.
+
+## Features
+- Dashboard
+- Task Management
+- Add Tasks
+- Calendar
+- Subjects
+- Statistics
+- Profile Page
+
+## Tech Stack
+- React
+- Tailwind CSS
+- React Router
+- JSON Server
+
+## Installation
+
+```bash
+npm install
+npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home'
-import Dashboard from './Pages/Dashboard';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-
-  );
-}
-export default App;
 // Import Browser Router tools
 import {
   BrowserRouter,
